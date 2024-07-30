9999c37a25123bda5ccc48039d11a0af3eedd820
DESKTOP-2JIU9FE\admin\Desktop\dev <hatgaji1240@gmail.com>
2024-07-30 21:46:18 +0900

    1. init

---
+++ vitf_web/src/main/java/com/vitf/web/VitfWebApplication.java
@@ -0,0 +1,13 @@
+package com.vitf.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitfWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitfWebApplication.class, args);
+	}
+
+}
